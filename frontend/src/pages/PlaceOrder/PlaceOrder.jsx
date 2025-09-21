478efe80cf1c134ce39990ea9275ab0d7838ae6f
Satyam <satyam10april02@gmail.com>
2025-09-21 20:27:22 +0530

    Updated ui

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -30,20 +30,20 @@ const PlaceOrder = () => {
         <div className="cart-total">
           <h2>Cart Totals</h2>
           <div>
-           <div className="cart-total-details">
-                            <p>Subtotal</p>
-                            <p>${getTotalCartAmount()}</p>
-                        </div>
-                        <hr />
-                        <div className="cart-total-details">
-                            <p>Delivery Fee</p>
-                            <p>${getTotalCartAmount()===0?0:2}</p>
-                        </div>
-                        <hr />
-                        <div className="cart-total-details">
-                            <b>Total</b>
-                            <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
-                        </div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
+            </div>
           </div>
           <button>PROCEED TO PAYMENT</button>
         </div>
